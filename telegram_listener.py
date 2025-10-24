bbd836393c74133ffd6d2540a8abc43867531303
VocoPoco <vik.penev@gmail.com>
2025-10-24 14:37:19 +0200

    fix: refactored fetch_all_messages_from_date

--- telegram_listener.py
+++ telegram_listener.py
@@ -1,8 +1,12 @@
 from pyrogram import Client, filters
 from shared.constants import TELEGRAM_APP_ID, TELEGRAM_API_HASH, TELEGRAM_GROUP_ID, LAST_MESSAGE_FILE
-from shared.parser import parse_signal
+from shared.parser import SignalParser, parse_signal
+from storage.file_manager import FileManager
+
 import time
 import asyncio
+from datetime import datetime, timezone
+
 
 class TelegramListener:
     """
@@ -13,9 +17,16 @@ class TelegramListener:
     def __init__(self, queue, app_name: str = "telegram_bot"):
         self.queue = queue
         self.app = Client(app_name, api_id=TELEGRAM_APP_ID, api_hash=TELEGRAM_API_HASH)
-        print("Telegram client initialized.")
-        self.__retrieve_last_message_id()
+        self.parser = SignalParser()
+        self.file_manager = FileManager(last_id_path=LAST_MESSAGE_FILE)
+        self.__retrieve_last_message_id_from_file()
 
+
+    async def run(self):
+        """Start the Telegram client and begin polling messages."""
+        await self.app.start()
+        print("Telegram listener started.")
+        await self.poll_channel()
 
     async def poll_channel(self):
         """Manually fetch the latest message and check for new signals."""
@@ -26,13 +37,13 @@ class TelegramListener:
                         if self.last_message_id != message.id:
                             print(f"New message ID {message.id} detected (last was {self.last_message_id})")
                             self.last_message_id = message.id 
-                            self.__store_last_message_id()
+                            self.__store_last_message_id_in_file(self.last_message_id)
 
                             text = (message.text or message.caption or "").strip()
 
                             if text:  
                                 print(f"New message detected: {text}")
-                                sig = parse_signal(text)
+                                sig = self.parser.parse(text)
                                 if sig and self.queue:
                                     print(f"[SIGNAL RECEIVED] {sig}")
                                     await self.queue.put(sig) 
@@ -42,12 +53,6 @@ class TelegramListener:
 
             await asyncio.sleep(3)
 
-
-    async def run(self):
-        """Start the Telegram client and begin polling messages."""
-        await self.app.start()
-        print("Telegram listener started.")
-        await self.poll_channel()
 
 
     def print_last_message(self):
@@ -68,36 +73,28 @@ class TelegramListener:
 
 
     def check_group_type(self):
-        app = Client("telegram_bot", api_id=TELEGRAM_APP_ID, api_hash=TELEGRAM_API_HASH)
-        app.start()
-        chat = app.get_chat(-1003054653270)
+        self.app.start()
+        chat = self.app.get_chat(-1003054653270)
         print(f"Chat type: {chat.type}")
-        app.stop()
+        self.app.stop()
 
 
-    def __retrieve_last_message_id(self):
-        """Retrieve the last processed message ID from file."""
+    def fetch_all_messages_from_date(self, date: datetime = datetime(2025, 4, 25, tzinfo=timezone.utc)):
+        self.app.start()
+        out = []
         try:
-            with open(LAST_MESSAGE_FILE, "r") as f:
-                self.last_message_id = int(f.read().strip())
-                print(f"Retrieved last message ID: {self.last_message_id}")
-        except FileNotFoundError:
-            self.last_message_id = None
-            print("No last message ID file found. Starting fresh.")
-        except Exception as e:
-            print(f"Error retrieving last message ID: {e}")
-            self.last_message_id = None
-
-
-    def __store_last_message_id(self):
-        """Store the last processed message ID to file."""
-        try:
-            with open(LAST_MESSAGE_FILE, "w") as f:
-                f.write(str(self.last_message_id))
-                print(f"Stored last message ID: {self.last_message_id}")
-        except Exception as e:
-            print(f"Error storing last message ID: {e}")
-
+            messages = self.app.get_chat_history(TELEGRAM_GROUP_ID, limit=0)
+            for message in messages:
+                if not message.date:
+                    continue
+                if message.date.replace(tzinfo=timezone.utc) < date:
+                    break
+                text = (message.text or message.caption or "").strip()
+                if text:
+                    out.append({"id": message.id, "date": message.date, "text": text})
+        finally:
+            self.app.stop()
+        return out
 
 if __name__ == "__main__":
     listener = TelegramListener(queue=None)
